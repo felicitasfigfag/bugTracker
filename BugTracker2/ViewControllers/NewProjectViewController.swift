a73127216bd5dc18ce8d8cb9705ac7146aecde80
Felicitas Figueroa Fagalde <felicitasfigfag@Felicitass-MacBook-Air.local>
2022-08-29 12:21:22 +0200

    Interface ready

--- BugTracker2/ViewControllers/NewProjectViewController.swift
+++ BugTracker2/ViewControllers/NewProjectViewController.swift
@@ -8,24 +8,18 @@
 import UIKit
 
 class NewProjectViewController: UIViewController {
-
+    
+    @IBOutlet var picker : UIPickerView!
+    
+    let team = ["Felicitas", "Gregorio", "Nick", "Joe", "Kevin"]
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .white
         self.title = "New Project"
-
         // Do any additional setup after loading the view.
     }
     
 
-    /*
-    // MARK: - Navigation
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
